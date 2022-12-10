3ea98fb23cd93344c28c1aa557ba709cb4bc8017
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-10 19:43:17 +0700

    create components template

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Results from "./Results";
+export { Header, Navbar, Results };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import { Header, Navbar, Results } from "../components"
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      
+      <Header />
+      <Navbar/>
+      <Results/>
     </div>
   )
 }
