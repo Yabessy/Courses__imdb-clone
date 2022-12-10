76d31e0a4ddaa8cde1b2fca6be97996a93c03046
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-10 23:16:51 +0700

    fixing initial url error

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home({ results }: any) {
 }
 
 export async function getServerSideProps(context: any) {
-  const genre = context.query.genre || "trending"
+  const genre = context.query.genre || "fetchTrending"
   // @ts-ignore
   const request = await fetch(`https://api.themoviedb.org/3${requests[genre].url}`)
   .then((res) => res.json())
